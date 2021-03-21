4288b6c9dc5f0bcfcfc54e46503346e7211940f8
maryakinyi <owuorakinyimary@gmail.com>
2021-03-21 22:50:08 +0300

    assignment3

---
+++ assignment.js
@@ -0,0 +1,21 @@
+function getGrade() {
+    var marks = 87
+    if (marks >= 80) {
+        console.log("A");
+    } else
+    if (marks >= 70) {
+        console.log("B")
+    } else if (marks >= 60) {
+        console.log("C");
+    } else if (marks >= 50) {
+        console.log("D");
+    } else if (marks <= 49) {
+        console.log("E");
+    } else if (marks = "F") {
+        console.log("F");
+
+    }
+
+
+
+}
